c63988f56313df012471d2ce9ed40b849881c288
kroberts <kroberts@netspend.com>
2016-04-18 16:56:28 -0500

    Moved heroes out into a separate component and have it injected into app component.

---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice Guy" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneto" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynamo" },
+  { "id": 18, "name": "Dr. Octopus" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Missappear" }
+];
