87697e626de25bd2782e5a3beb29ce5843b2e1f4
kroberts <kroberts@netspend.com>
2016-04-19 17:02:16 -0500

    Factored out code from AppComponent and turned it into a router component. Added dashboard and actual routes.

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -10,6 +10,12 @@ export class HeroService {
     return Promise.resolve(HEROES);
   }
 
+  getHero(id: number) {
+    return Promise.resolve(HEROES).then(
+        heroes => heroes.filter(hero => hero.id === id)[0]
+    );
+  }
+
   getHeroesSlowly() {
     return new Promise<Hero[]>(resolve =>
       setTimeout(() => resolve(HEROES), 1500) // Will resolve after 1.5 seconds. Simulate a server response time
